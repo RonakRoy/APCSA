9e31087aa7bdad2ea132063440588a6530a74637
Ronak Roy <ronak.r123@gmail.com>
2018-02-02 13:57:39 -0800

    Setup Eclipse

--- src/net/sduhsd/royr6099/unit0/Exercise1.java
+++ src/net/sduhsd/royr6099/unit0/Exercise1.java
@@ -4,7 +4,7 @@ public class Exercise1 {
 	public static void main(String[] args) {
 		System.out.println("a\ta^2\ta^3");
 		for (int i = 1; i <= 10; i++) {
-			System.out.println(i+"\t"+(i*i)+"\t"+(i*i*i));
+			System.out.println(i + "\t" + (i*i) + "\t" + (i*i*i));
 		}
 	}
 }
